dab5329adec93ddf8ecea2eb21b37c648de388a1
Rizasto <Rizasto@gmail.com>
2026-03-26 11:19:25 +0300

    Добавил базовый функционал по добавлению платежа

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 
+from app.api.v1.router import router as api_router
 from app.core.config import get_settings
 
 settings = get_settings()
 
 app = FastAPI(title=settings.APP_NAME)
+
+app.include_router(api_router)
 
 
 @app.get("/health")
